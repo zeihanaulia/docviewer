dbac0203b5db310acd4103444dd2c4d7471c9b29
zeihanaulia <zeihan.aulia@outlook.com>
2024-04-17 17:21:29 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,109 @@
+API Doc Viewer
+=============
+
+Description
+-----------
+
+This project is a web application that provides an interface for rendering and viewing API documentation. It supports both OpenAPI and AsyncAPI specifications. The application checks the version of AsyncAPI documents and displays a message if the version is unsupported (versions 3.x and above). The web interface uses the AsyncAPI React Component to render the API documentation dynamically from provided specifications.
+
+Features
+--------
+
+-   Support for OpenAPI and AsyncAPI specifications.
+-   Dynamic rendering of API documentation.
+-   Version checking for AsyncAPI to ensure compatibility.
+-   User-friendly error handling for unsupported API specification versions.
+
+Getting Started
+---------------
+
+### Prerequisites
+
+Ensure you have the following installed:
+
+-   [Go](https://golang.org/dl/) (Version 1.20 or later recommended)
+-   Fiber web framework for Go
+
+### Installation
+
+1.  Clone the repository:
+
+    ```
+    bashCopy code
+    `git clone https://your-repository-url
+    cd your-repository-directory
+    `
+
+    ```
+
+2.  Install the required Go packages:
+
+    ```
+    bashCopy code
+    `go mod tidy
+    `
+
+    ```
+
+### Running the Application
+
+To run the application, execute:
+
+```
+bashCopy code
+`go run main.go
+`
+
+```
+
+This starts the server on `http://localhost:8080`. You can access the API documentation interfaces by navigating to specific URLs (detailed in the Usage section below).
+
+Usage
+-----
+
+The application provides the following endpoints:
+
+### `/docs/:type/*`
+
+-   **Description**: Serve API documentation based on the specification type.
+-   **Method**: `GET`
+-   **URL Path**: `/docs/:type/{spec-url}`
+    -   `:type` - The type of API specification (`openapi` or `asyncapi`)
+    -   `{spec-url}` - The URL encoded path to the API specification file
+-   **Example**:
+    -   OpenAPI: `http://localhost:8080/docs/openapi/https://example.com/path/to/openapi.yaml`
+    -   AsyncAPI (Supported Versions): `http://localhost:8080/docs/asyncapi/https://example.com/path/to/asyncapi.yaml`
+
+### Error Handling
+
+-   **Unsupported AsyncAPI Versions**: Accessing an AsyncAPI document with a version starting with "3." will render a page from `unsupported_version.html`, informing the user that the version is not supported.
+
+Dependencies
+------------
+
+-   **Fiber**: Web framework used for routing and handling HTTP requests.
+-   **Go-YAML**: Used for YAML parsing.
+-   **Template**: Standard library used for rendering HTML templates.
+
+Contributing
+------------
+
+Contributions are what make the open-source community such an amazing place to learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+1.  Fork the Project
+2.  Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3.  Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4.  Push to the Branch (`git push origin feature/AmazingFeature`)
+5.  Open a Pull Request
+
+License
+-------
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+Contact
+-------
+
+Your Name - example@example.com
+
+Project Link: <https://github.com/zeihanaulia/docviewer>
---
+++ main.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"gopkg.in/yaml.v2"
+)
+
+// Define a struct to hold our template
+type Template struct {
+	templates *template.Template
+}
+
+// Parse and load templates on initialization
+func NewTemplate() *Template {
+	return &Template{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
+
+// Render template utility function
+func (t *Template) Render(c *fiber.Ctx, name string, data interface{}) error {
+	c.Type("html")
+	return t.templates.ExecuteTemplate(c.Response().BodyWriter(), name, data)
+}
+
+func convertInterfaceMapToJSONMap(in interface{}) (interface{}, error) {
+	switch x := in.(type) {
+	case map[interface{}]interface{}:
+		m := map[string]interface{}{}
+		for k, v := range x {
+			ks, ok := k.(string)
+			if !ok {
+				return nil, errors.New("key is not a string")
+			}
+			var err error
+			m[ks], err = convertInterfaceMapToJSONMap(v)
+			if err != nil {
+				return nil, err
+			}
+		}
+		return m, nil
+	case []interface{}:
+		for i, v := range x {
+			var err error
+			x[i], err = convertInterfaceMapToJSONMap(v)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+	return in, nil
+}
+
+func convertYAMLToJSON(data []byte) ([]byte, error) {
+	var yamlObj interface{}
+	if err := yaml.Unmarshal(data, &yamlObj); err != nil {
+		return nil, err
+	}
+
+	jsonReadyObj, err := convertInterfaceMapToJSONMap(yamlObj)
+	if err != nil {
+		return nil, err
+	}
+
+	jsonData, err := json.Marshal(jsonReadyObj)
+	if err != nil {
+		return nil, err
+	}
+
+	return jsonData, nil
+}
+
+func handleDocumentation(c *fiber.Ctx, t *Template, specType, templateName string) error {
+	specURL := c.Params("*")
+	resp, err := http.Get(specURL)
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).SendString("Failed to retrieve the specification")
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).SendString("Failed to read the specification")
+	}
+
+	var jsonData []byte
+	if strings.HasSuffix(specURL, ".yaml") || strings.HasSuffix(specURL, ".yml") {
+		jsonData, err = convertYAMLToJSON(data)
+		if err != nil {
+			return c.Status(http.StatusInternalServerError).SendString("Failed to convert YAML to JSON: " + err.Error())
+		}
+	} else {
+		jsonData = data // Assume the data is already JSON
+	}
+
+	// Check AsyncAPI version if specType is "asyncapi"
+	if specType == "asyncapi" {
+		var spec map[string]interface{}
+		if err := json.Unmarshal(jsonData, &spec); err != nil {
+			return c.Status(http.StatusInternalServerError).SendString("Failed to parse JSON: " + err.Error())
+		}
+		version, ok := spec["asyncapi"].(string)
+		if ok && strings.HasPrefix(version, "3.") {
+			return t.Render(c, "unsupported_version.html", nil)
+		}
+	}
+
+	encodedData := base64.StdEncoding.EncodeToString(jsonData)
+	return t.Render(c, templateName, map[string]interface{}{
+		"EncodedSpecContent": encodedData,
+	})
+}
+
+func main() {
+	app := fiber.New()
+	app.Use(logger.New())
+	tmpl := NewTemplate()
+
+	// Combined route for API documentation
+	app.Get("/docs/:type/*", func(c *fiber.Ctx) error {
+		specType := c.Params("type")
+		switch specType {
+		case "openapi", "asyncapi":
+			templateName := specType + ".html" // assumes templates are named openapi.html and asyncapi.html
+			return handleDocumentation(c, tmpl, specType, templateName)
+		default:
+			return c.Status(fiber.StatusBadRequest).SendString("Unsupported specification type")
+		}
+	})
+
+	log.Fatal(app.Listen(":8080"))
+}
